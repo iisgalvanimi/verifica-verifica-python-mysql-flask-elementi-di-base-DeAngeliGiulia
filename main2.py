0c9ea918d7a969a3808671dc0d12c7d9b64902f2
Giulia De Angeli <deangeli.giulia@iisgalvanimi.edu.it>
2024-11-08 11:47:53 +0000

    commit 3

--- main2.py
+++ main2.py
@@ -33,5 +33,38 @@ def get_data():
             cursor.close()
             conn.close()
 
+@app.route('/dati/crea', methods=['POST'])
+def create_animal():
+    data = request.get_json()
+
+    nome_comune = data.get('Nome_Comune')
+    famiglia = data.get('Famiglia')
+    ambiente = data.get('Ambiente')
+    dimensioni = data.get('Dimensioni')
+    alimentazione = data.get('Alimentazione')
+
+    if not nome_comune or not famiglia or not ambiente or not dimensioni or not alimentazione:
+        return jsonify({"errore": "Tutti i campi (Nome_Comune, Famiglia, Ambiente, Dimensioni, Alimentazione) sono obbligatori"}), 400
+
+    try:
+        conn = mysql.connector.connect(**db_config)
+        cursor = conn.cursor()
+
+        query = "INSERT INTO Pesci (Nome_Comune, Famiglia, Ambiente, Dimensioni, Alimentazione) VALUES (%s, %s, %s, %s, %s)"
+
+        cursor.execute(query, (nome_comune, famiglia, ambiente, dimensioni, alimentazione))
+        conn.commit()
+
+        return jsonify({"successo": "Nuovo animale creato", "id": cursor.lastrowid}), 201
+    
+    except Error as e:
+        print("Errore nella connessione al database:", e)
+        return jsonify({"errore": "Impossibile inserire l'animale nel database"}), 500
+    
+    finally:
+        if conn.is_connected():
+            cursor.close()
+            conn.close()
+
 if __name__ == '__main__':
     app.run(debug=True)
